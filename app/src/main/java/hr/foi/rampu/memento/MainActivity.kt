b1af0fa1dbc24e411853a6a76b810109704afe1b
Mislav Matijević <mislav.mim@gmail.com>
2023-10-15 19:12:23 +0200

    Further improve MainPageAdapter. Now it dynamically takes in FragmentItem objects from the outside, reducing its responsibility and allowing its behaviour to be extended in the future.

--- app/src/main/java/hr/foi/rampu/memento/MainActivity.kt
+++ app/src/main/java/hr/foi/rampu/memento/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import hr.foi.rampu.memento.adapters.MainPagerAdapter
+import hr.foi.rampu.memento.fragments.CompletedFragment
+import hr.foi.rampu.memento.fragments.NewsFragment
+import hr.foi.rampu.memento.fragments.PendingFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +21,30 @@ class MainActivity : AppCompatActivity() {
         tabLayout = findViewById(R.id.tabs)
         viewPager2 = findViewById(R.id.viewpager)
 
-        viewPager2.adapter = MainPagerAdapter(supportFragmentManager, lifecycle)
+        val mainPagerAdapter = MainPagerAdapter(supportFragmentManager, lifecycle)
+
+        mainPagerAdapter.addFragment(
+            MainPagerAdapter.FragmentItem(
+                R.string.tasks_pending,
+                R.drawable.baseline_assignment_late_24,
+                PendingFragment::class
+            )
+        )
+        mainPagerAdapter.addFragment(
+            MainPagerAdapter.FragmentItem(
+                R.string.tasks_completed,
+                R.drawable.baseline_assignment_turned_in_24,
+                CompletedFragment::class
+            )
+        )
+        mainPagerAdapter.addFragment(
+            MainPagerAdapter.FragmentItem(
+                R.string.news,
+                R.drawable.baseline_wysiwyg_24,
+                NewsFragment::class
+            )
+        )
+
+        viewPager2.adapter = mainPagerAdapter
     }
 }
--- app/src/main/java/hr/foi/rampu/memento/adapters/MainPageAdapter.kt
+++ app/src/main/java/hr/foi/rampu/memento/adapters/MainPageAdapter.kt
@@ -4,10 +4,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import hr.foi.rampu.memento.R
-import hr.foi.rampu.memento.fragments.CompletedFragment
-import hr.foi.rampu.memento.fragments.NewsFragment
-import hr.foi.rampu.memento.fragments.PendingFragment
 import kotlin.reflect.KClass
 
 class MainPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
@@ -15,17 +11,11 @@ class MainPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
 
     data class FragmentItem(val titleRes: Int, val iconRes: Int, val fragmentClass: KClass<*>)
 
-    val fragmentItems = listOf(
-        FragmentItem(
-            R.string.tasks_pending, R.drawable.baseline_assignment_late_24, PendingFragment::class
-        ),
-        FragmentItem(
-            R.string.tasks_completed, R.drawable.baseline_assignment_turned_in_24, CompletedFragment::class
-        ),
-        FragmentItem(
-            R.string.news, R.drawable.baseline_wysiwyg_24, NewsFragment::class
-        )
-    )
+    val fragmentItems = ArrayList<FragmentItem>()
+
+    fun addFragment(fragment: FragmentItem) {
+        fragmentItems.add(fragment)
+    }
 
     override fun getItemCount(): Int = fragmentItems.size
 
