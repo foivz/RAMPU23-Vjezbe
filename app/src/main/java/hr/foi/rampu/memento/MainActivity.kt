bf23801f84681e9343e8440cdf01453ce175123b
Mislav Matijević <mislav.mim@gmail.com>
2023-10-15 19:06:52 +0200

    Use MainPagerAdapter as ViewPager2's adapter. Now, fragments can be swiped through.

--- app/src/main/java/hr/foi/rampu/memento/MainActivity.kt
+++ app/src/main/java/hr/foi/rampu/memento/MainActivity.kt
@@ -1,11 +1,23 @@
 package hr.foi.rampu.memento
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import hr.foi.rampu.memento.adapters.MainPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager2: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tabs)
+        viewPager2 = findViewById(R.id.viewpager)
+
+        viewPager2.adapter = MainPagerAdapter(supportFragmentManager, lifecycle)
     }
 }
