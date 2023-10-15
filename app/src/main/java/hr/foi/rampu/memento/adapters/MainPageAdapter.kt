9b8bef1631f60e427a5bd54d7ada7b4ef92fdfd6
Mislav Matijević <mislav.mim@gmail.com>
2023-10-15 19:04:45 +0200

    Implement MainPagerAdapter.

--- app/src/main/java/hr/foi/rampu/memento/adapters/MainPageAdapter.kt
+++ app/src/main/java/hr/foi/rampu/memento/adapters/MainPageAdapter.kt
@@ -4,14 +4,28 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import hr.foi.rampu.memento.R
+import hr.foi.rampu.memento.fragments.CompletedFragment
+import hr.foi.rampu.memento.fragments.NewsFragment
+import hr.foi.rampu.memento.fragments.PendingFragment
 
 class MainPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+
+    val titleList = listOf(R.string.pending_fragment, R.string.completed_fragment, R.string.news_fragment)
+    val iconList = listOf(
+        R.drawable.baseline_assignment_late_24,
+        R.drawable.baseline_assignment_turned_in_24,
+        R.drawable.baseline_wysiwyg_24
+    )
+
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
+        return when (position) {
+            0 -> PendingFragment()
+            1 -> CompletedFragment()
+            else -> NewsFragment()
+        }
     }
 }
