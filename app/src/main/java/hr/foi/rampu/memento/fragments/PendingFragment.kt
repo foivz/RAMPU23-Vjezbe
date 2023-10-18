f20887b019c2a1f7365b2ac138a8be3215538a6f
Mislav Matijević <mmatijevi@foi.hr>
2023-10-18 13:28:53 +0200

    Add RecyclerView View with reference in code.

--- app/src/main/java/hr/foi/rampu/memento/fragments/PendingFragment.kt
+++ app/src/main/java/hr/foi/rampu/memento/fragments/PendingFragment.kt
@@ -5,14 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.memento.R
 
 class PendingFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pending, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_pending_tasks)
     }
 }
