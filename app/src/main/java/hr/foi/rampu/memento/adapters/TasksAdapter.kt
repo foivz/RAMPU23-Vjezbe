dbb24fe2342cffb766b0472941515eb2e345d2d5
Mislav Matijević <mmatijevi@foi.hr>
2023-10-18 13:41:28 +0200

    Apply category colors to task items.

--- app/src/main/java/hr/foi/rampu/memento/adapters/TasksAdapter.kt
+++ app/src/main/java/hr/foi/rampu/memento/adapters/TasksAdapter.kt
@@ -1,9 +1,11 @@
 package hr.foi.rampu.memento.adapters
 
 import android.view.LayoutInflater
+import android.view.SurfaceView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.graphics.toColorInt
 import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.memento.R
 import hr.foi.rampu.memento.entities.Task
@@ -17,15 +19,18 @@ class TasksAdapter(private val tasksList: List<Task>) :
         private val sdf: SimpleDateFormat = SimpleDateFormat("dd.MM.yyyy. HH:mm", Locale.ENGLISH)
         private val taskName: TextView
         private val taskDueDate: TextView
+        private val taskCategoryColor: SurfaceView
 
         init {
             taskName = view.findViewById(R.id.tv_task_name)
             taskDueDate = view.findViewById(R.id.tv_task_due_date)
+            taskCategoryColor = view.findViewById(R.id.sv_task_category_color)
         }
 
         fun bind(task: Task) {
             taskName.text = task.name
             taskDueDate.text = sdf.format(task.dueDate)
+            taskCategoryColor.setBackgroundColor(task.category.color.toColorInt())
         }
     }
 
