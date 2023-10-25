9964d155dcff9364bf7290d1979e7bd4ec0e67af
Mislav Matijević <mmatijevi@foi.hr>
2023-10-25 13:33:47 +0200

    Make create task button open a new task dialog.

--- app/src/main/java/hr/foi/rampu/memento/fragments/PendingFragment.kt
+++ app/src/main/java/hr/foi/rampu/memento/fragments/PendingFragment.kt
@@ -1,5 +1,6 @@
 package hr.foi.rampu.memento.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,12 +8,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import hr.foi.rampu.memento.R
 import hr.foi.rampu.memento.adapters.TasksAdapter
 import hr.foi.rampu.memento.helpers.MockDataLoader
 
 class PendingFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var btnCreateTask: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,5 +29,21 @@ class PendingFragment : Fragment() {
         recyclerView = view.findViewById(R.id.rv_pending_tasks)
         recyclerView.adapter = TasksAdapter(MockDataLoader.getDemoData())
         recyclerView.layoutManager = LinearLayoutManager(view.context)
+
+        btnCreateTask = view.findViewById(R.id.fab_pending_fragment_create_task)
+        btnCreateTask.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val newTaskDialogView = LayoutInflater
+            .from(context)
+            .inflate(R.layout.new_task_dialog, null)
+
+        AlertDialog.Builder(context)
+            .setView(newTaskDialogView)
+            .setTitle(getString(R.string.create_a_new_task))
+            .show()
     }
 }
