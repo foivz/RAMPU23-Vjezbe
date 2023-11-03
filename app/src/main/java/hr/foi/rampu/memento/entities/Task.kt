e12a593180217b4169754e1574074aaf5f97ba05
Mislav Matijević <mmatijevi@foi.hr>
2023-11-03 10:37:45 +0100

    Implement a DateConverter. Task class contains a Date type property that needed conversion to a timestamp in order to be stored in the database.

--- app/src/main/java/hr/foi/rampu/memento/entities/Task.kt
+++ app/src/main/java/hr/foi/rampu/memento/entities/Task.kt
@@ -5,6 +5,8 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import hr.foi.rampu.memento.converters.DateConverter
 import java.util.Date
 
 @Entity(
@@ -16,6 +18,7 @@ import java.util.Date
         onDelete = ForeignKey.RESTRICT
     )]
 )
+@TypeConverters(DateConverter::class)
 data class Task(
     @PrimaryKey val id: Int,
     val name: String,
