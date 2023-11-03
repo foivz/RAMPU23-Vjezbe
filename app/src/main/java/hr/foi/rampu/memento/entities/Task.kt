db406c421a5258710b0c31626eb9ad7a21e550ad
Mislav Matijević <mmatijevi@foi.hr>
2023-11-03 10:32:34 +0100

    Make Task be a Room database Entity. Connected it to the TaskCategory class as a Foreign Key.

--- app/src/main/java/hr/foi/rampu/memento/entities/Task.kt
+++ app/src/main/java/hr/foi/rampu/memento/entities/Task.kt
@@ -1,10 +1,28 @@
 package hr.foi.rampu.memento.entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 import java.util.Date
 
-data class Task(
-    val name: String,
-    val dueDate: Date,
-    val category: TaskCategory,
-    val completed: Boolean
+@Entity(
+    tableName = "tasks",
+    foreignKeys = [ForeignKey(
+        entity = TaskCategory::class,
+        parentColumns = ["id"],
+        childColumns = ["category_id"],
+        onDelete = ForeignKey.RESTRICT
+    )]
 )
+data class Task(
+    @PrimaryKey val id: Int,
+    val name: String,
+    @ColumnInfo(name = "due_date") val dueDate: Date,
+    @ColumnInfo(name = "category_id") val categoryId: Int,
+    val completed: Boolean
+) {
+    @Ignore
+    lateinit var category: TaskCategory
+}
