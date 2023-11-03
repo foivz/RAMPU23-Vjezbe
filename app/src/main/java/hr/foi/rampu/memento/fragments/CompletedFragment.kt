71e77f9ad037f71b564f1c1ab94897e3238924b9
Mislav Matijević <mmatijevi@foi.hr>
2023-11-03 11:40:58 +0100

    Show completed tasks on the CompletedFragment. Reused task displaying logic from PendingFragment, only now tasks marked as completed are fetched from the database.

--- app/src/main/java/hr/foi/rampu/memento/fragments/CompletedFragment.kt
+++ app/src/main/java/hr/foi/rampu/memento/fragments/CompletedFragment.kt
@@ -5,14 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.memento.R
+import hr.foi.rampu.memento.adapters.TasksAdapter
+import hr.foi.rampu.memento.database.TasksDatabase
 
 class CompletedFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_completed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_completed_tasks)
+        val completedTasks = TasksDatabase.getInstance().getTasksDao().getAllTasks(completed = true)
+        recyclerView.adapter = TasksAdapter(completedTasks.toMutableList())
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
     }
 }
