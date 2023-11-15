ed8e218cb85478301e432e0dba48e07f07760c45
Mislav Matijević <mmatijevi@foi.hr>
2023-11-15 13:15:58 +0100

    Implement basic TaskTimerService logic. It contains a list of tasks, a flag whether the task is started and getting the task in question from the database. Starting of the foreground service with a notification itself is not yet implemented.

--- app/src/main/java/hr/foi/rampu/memento/services/TaskTimerService.kt
+++ app/src/main/java/hr/foi/rampu/memento/services/TaskTimerService.kt
@@ -2,11 +2,36 @@ package hr.foi.rampu.memento.services
 
 import android.app.Service
 import android.content.Intent
-import android.os.IBinder
+import hr.foi.rampu.memento.database.TasksDatabase
+import hr.foi.rampu.memento.entities.Task
+import java.util.Date
 
 class TaskTimerService : Service() {
+    private val tasks = mutableListOf<Task>()
+    private var started: Boolean = false
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val taskId = intent.getIntExtra("task_id", -1)
+        val isCanceled = intent.getBooleanExtra("cancel", false)
+
+        TasksDatabase.buildInstance(applicationContext)
+        val task = TasksDatabase.getInstance().getTasksDao().getTask(taskId)
+
+        if (tasks.contains(task)) {
+            if (isCanceled) {
+                tasks.remove(task)
+            }
+        } else if (task.dueDate > Date()) {
+            tasks.add(task)
+
+            if (!started) {
+                TODO("Start service")
+                started = true
+            }
+        }
+
+        return START_NOT_STICKY
     }
+
+    override fun onBind(intent: Intent) = null
 }
