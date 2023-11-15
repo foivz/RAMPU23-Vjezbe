6a07119f34b56e3c6161cadef34b2a89ede5b6e6
Mislav Matijević <mmatijevi@foi.hr>
2023-11-15 13:08:33 +0100

    Add TaskTimerService. Right click on new `services` package: New -> Service -> Service, check enabled and exported.

---
+++ app/src/main/java/hr/foi/rampu/memento/services/TaskTimerService.kt
@@ -0,0 +1,12 @@
+package hr.foi.rampu.memento.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class TaskTimerService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
