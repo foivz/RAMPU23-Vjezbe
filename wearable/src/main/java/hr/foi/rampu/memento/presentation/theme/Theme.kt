8663edf70b83715f6ad51aa7234a2b86e21706a2
Mislav Matijević <mmatijevi@foi.hr>
2023-11-20 11:28:51 +0100

    Create new wearable module. This new module will serve in the context of creating a new WearOS Memento application.

---
+++ wearable/src/main/java/hr/foi/rampu/memento/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package hr.foi.rampu.memento.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun MementoTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = wearColorPalette,
+        typography = Typography,
+        // For shapes, we generally recommend using the default Material Wear shapes which are
+        // optimized for round and non-round devices.
+        content = content
+    )
+}
