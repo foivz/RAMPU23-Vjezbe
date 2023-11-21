6303bd3df08d4fdada3ae47d100b22af2438aa4e
Mislav Matijević <mmatijevi@foi.hr>
2023-11-21 11:02:05 +0100

    Improve wearable list design. Implement progress bar if no tasks yet present. Add blue coloring to everything. Various smaller UI improvements.

--- wearable/src/main/java/hr/foi/rampu/memento/presentation/theme/Color.kt
+++ wearable/src/main/java/hr/foi/rampu/memento/presentation/theme/Color.kt
@@ -3,17 +3,16 @@ package hr.foi.rampu.memento.presentation.theme
 import androidx.compose.ui.graphics.Color
 import androidx.wear.compose.material.Colors
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val MementoBlue = Color(0xFFCAE2FF)
+val MementoDarkerBlue = Color(0xFFAEC8FC)
+val MementoAccent = Color(0xFFFF5722)
 val Red400 = Color(0xFFCF6679)
 
 internal val wearColorPalette: Colors = Colors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200,
-    secondaryVariant = Teal200,
+    primary = MementoBlue,
+    primaryVariant = MementoDarkerBlue,
+    secondary = MementoAccent,
+    secondaryVariant = MementoAccent,
     error = Red400,
     onPrimary = Color.Black,
     onSecondary = Color.Black,
