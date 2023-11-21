3b3e1b7ec15d9f760800d4f2bab8d22945124fa3
Mislav Matijević <mmatijevi@foi.hr>
2023-11-21 09:37:40 +0100

    Create MementoApp Composable. To be used to display tasks in a list on a Wear device. MainActivity now only calls MementoApp.

---
+++ wearable/src/main/java/hr/foi/rampu/memento/presentation/MementoApp.kt
@@ -0,0 +1,8 @@
+package hr.foi.rampu.memento.presentation
+
+import androidx.compose.runtime.Composable
+import hr.foi.rampu.memento.presentation.models.Task
+
+@Composable
+fun MementoApp(tasks: List<Task>) {
+}
