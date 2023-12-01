43925bbc45e07c604d47ea6eddf2a00f89fef12f
Mislav Matijević <mmatijevi@foi.hr>
2023-12-01 15:56:53 +0100

    Implement switching to dark mode.

--- app/src/main/java/hr/foi/rampu/memento/PreferencesActivity.kt
+++ app/src/main/java/hr/foi/rampu/memento/PreferencesActivity.kt
@@ -1,11 +1,34 @@
 package hr.foi.rampu.memento
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
 
-class PreferencesActivity : AppCompatActivity() {
+class PreferencesActivity : AppCompatActivity(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
+
+    companion object {
+        fun switchDarkMode(isDarkModeSelected: Boolean?) {
+            if (isDarkModeSelected == true) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferences)
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        preferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        when (key) {
+            "preference_dark_mode" -> switchDarkMode(sharedPreferences?.getBoolean(key, false))
+        }
     }
 }
