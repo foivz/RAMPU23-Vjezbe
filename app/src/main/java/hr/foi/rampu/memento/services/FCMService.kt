91dd7f4c5a68882bdc455e97c33193517ca58a7a
Mislav Matijević <mmatijevi@foi.hr>
2023-12-06 13:44:09 +0100

    Show notification on Firebase "news" message.

--- app/src/main/java/hr/foi/rampu/memento/services/FCMService.kt
+++ app/src/main/java/hr/foi/rampu/memento/services/FCMService.kt
@@ -1,16 +1,38 @@
 package hr.foi.rampu.memento.services
 
-import android.util.Log
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import hr.foi.rampu.memento.R
 
 class FCMService : FirebaseMessagingService() {
+    private var id = 0
+
     override fun onCreate() {
         FirebaseMessaging.getInstance().subscribeToTopic("news")
+
+        val channel =
+            NotificationChannel("news", "News Channel", NotificationManager.IMPORTANCE_HIGH)
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 
+    @SuppressLint("MissingPermission")
     override fun onMessageReceived(message: RemoteMessage) {
-        Log.i("MEMENTO_FCM_MSG", "Message data payload: ${message.data}")
+        val notification =
+            NotificationCompat.Builder(applicationContext, "news")
+                .setContentTitle(message.data["newNewsName"])
+                .setStyle(NotificationCompat.BigTextStyle().bigText(message.data["newsText"]))
+                .setSmallIcon(R.drawable.baseline_wysiwyg_24)
+                .build()
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(++id, notification)
+        }
     }
 }
