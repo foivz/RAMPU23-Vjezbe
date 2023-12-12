d679d90933046f0d0be1d11b74859202bc2b367a
Mislav Matijević <mmatijevi@foi.hr>
2023-12-12 16:01:45 +0100

    Implement topic notifications. Phone now subscribes to be notified when a topic gets notifications. Topics are sent by Firebase, and the phone reacts on receiving the Firebase's RemoteMessage for topics by creating a new notification in the "info" channel.

--- app/src/main/java/hr/foi/rampu/memento/PreferencesActivity.kt
+++ app/src/main/java/hr/foi/rampu/memento/PreferencesActivity.kt
@@ -2,14 +2,18 @@ package hr.foi.rampu.memento
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceManager
+import com.google.firebase.messaging.FirebaseMessaging
 
 const val RESULT_LANG_CHANGED = AppCompatActivity.RESULT_FIRST_USER
 
 class PreferencesActivity : AppCompatActivity(),
     SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private val possibleSubscriptions = listOf("RAMPU", "RWA", "RPP")
 
     companion object {
         fun switchDarkMode(isDarkModeSelected: Boolean?) {
@@ -32,11 +36,26 @@ class PreferencesActivity : AppCompatActivity(),
         when (key) {
             "preference_dark_mode" -> switchDarkMode(sharedPreferences?.getBoolean(key, false))
             "preference_language" -> notifyLanguageChangedAndClose()
+            "preference_subscribed_categories" -> manageFCMSubscriptions(
+                sharedPreferences?.getStringSet(key, setOf())!!
+            )
         }
     }
 
     private fun notifyLanguageChangedAndClose() {
         setResult(RESULT_LANG_CHANGED)
         finish()
+    }
+
+    private fun manageFCMSubscriptions(selectedTopics: Set<String>) {
+        possibleSubscriptions.forEach { availableTopic ->
+            if (selectedTopics.contains(availableTopic)) {
+                Log.i("MEMENTO_TOPICS", "Subscribing to $availableTopic")
+                FirebaseMessaging.getInstance().subscribeToTopic(availableTopic)
+            } else {
+                Log.i("MEMENTO_TOPICS", "Unsubscribing from $availableTopic")
+                FirebaseMessaging.getInstance().unsubscribeFromTopic(availableTopic)
+            }
+        }
     }
 }
--- app/src/main/java/hr/foi/rampu/memento/services/FCMService.kt
+++ app/src/main/java/hr/foi/rampu/memento/services/FCMService.kt
@@ -1,6 +1,7 @@
 package hr.foi.rampu.memento.services
 
 import android.annotation.SuppressLint
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -19,29 +20,46 @@ class FCMService : FirebaseMessagingService() {
     override fun onCreate() {
         FirebaseMessaging.getInstance().subscribeToTopic("news")
 
-        val channel =
+        val channelNews =
             NotificationChannel("news", "News Channel", NotificationManager.IMPORTANCE_HIGH)
+        val channelInfo =
+            NotificationChannel("info", "Info Channel", NotificationManager.IMPORTANCE_HIGH)
+
         val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
+
+        notificationManager.createNotificationChannel(channelNews)
+        notificationManager.createNotificationChannel(channelInfo)
     }
 
     @SuppressLint("MissingPermission")
     override fun onMessageReceived(message: RemoteMessage) {
-        val intentShow = Intent(this, NewsActivity::class.java).apply {
-            putExtra("news_name", message.data["newNewsName"])
+        lateinit var notification: Notification
+        val msgPayload = message.data["payload"]
+
+        if (msgPayload == "news") {
+            val intentShow = Intent(this, NewsActivity::class.java).apply {
+                putExtra("news_name", message.data["newNewsName"])
+            }
+
+            val openActivityIntent =
+                PendingIntent.getActivity(this, 0, intentShow, PendingIntent.FLAG_IMMUTABLE)
+
+            notification =
+                NotificationCompat.Builder(applicationContext, "news")
+                    .setContentTitle(message.data["newNewsName"])
+                    .setStyle(NotificationCompat.BigTextStyle().bigText(message.data["newsText"]))
+                    .setSmallIcon(R.drawable.baseline_wysiwyg_24)
+                    .setContentIntent(openActivityIntent)
+                    .setAutoCancel(true)
+                    .build()
+        } else {
+            notification =
+                NotificationCompat.Builder(applicationContext, "info")
+                    .setContentTitle(message.data["infoTitle"])
+                    .setStyle(NotificationCompat.BigTextStyle().bigText(message.data["infoText"]))
+                    .setSmallIcon(R.drawable.baseline_info_24)
+                    .build()
         }
-
-        val openActivityIntent =
-            PendingIntent.getActivity(this, 0, intentShow, PendingIntent.FLAG_IMMUTABLE)
-
-        val notification =
-            NotificationCompat.Builder(applicationContext, "news")
-                .setContentTitle(message.data["newNewsName"])
-                .setStyle(NotificationCompat.BigTextStyle().bigText(message.data["newsText"]))
-                .setSmallIcon(R.drawable.baseline_wysiwyg_24)
-                .setContentIntent(openActivityIntent)
-                .setAutoCancel(true)
-                .build()
 
         with(NotificationManagerCompat.from(this)) {
             notify(++id, notification)
